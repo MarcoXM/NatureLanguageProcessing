e330d82638069a9c65b09e45b569b98c620a8c90
MarcoXM <xwang423@fordham.edu>
2020-03-22 22:41:02 -0400

    seq2seq

---
+++ pytorch/text_translation/src/dataset.py
@@ -0,0 +1,45 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from torchtext.datasets import TranslationDataset, Multi30k
+from torchtext.data import Field, BucketIterator
+
+import spacy
+import numpy as np
+
+import random
+import math
+import time
+
+
+spacy_de = spacy.load('de')
+spacy_en = spacy.load('en')
+
+def tokenize_de(text):
+    """
+    Tokenizes German text from a string into a list of strings (tokens) and reverses it
+    """
+    return [tok.text for tok in spacy_de.tokenizer(text)][::-1]
+
+def tokenize_en(text):
+    """
+    Tokenizes English text from a string into a list of strings (tokens)
+    """
+    return [tok.text for tok in spacy_en.tokenizer(text)]
+
+
+
+SRC = Field(tokenize = tokenize_de, 
+            init_token = '<sos>', 
+            eos_token = '<eos>', 
+            lower = True)
+
+TRG = Field(tokenize = tokenize_en, 
+            init_token = '<sos>', 
+            eos_token = '<eos>', 
+            lower = True)
+train_data, valid_data, test_data = Multi30k.splits(exts = ('.de', '.en'), 
+                                                    fields = (SRC, TRG))
+SRC.build_vocab(train_data, min_freq = 2)
+TRG.build_vocab(train_data, min_freq = 2)
---
+++ pytorch/text_translation/src/train.py
@@ -0,0 +1,71 @@
+import torch
+from models import Encoder, Decoder, Seq2Seq 
+from dataset import SRC,TRG,train_data,valid_data,test_data
+import torch.nn as nn 
+import math 
+import time
+from utils import seed_everything, init_weights, count_parameters,train, evaluate,epoch_time
+from torchtext.data import BucketIterator
+import torch.optim as optim
+from tqdm import tqdm
+
+seed_everything(224)
+
+BATCH_SIZE = 128
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+INPUT_DIM = len(SRC.vocab)
+OUTPUT_DIM = len(TRG.vocab)
+ENC_EMB_DIM = 256
+DEC_EMB_DIM = 256
+HID_DIM = 512
+N_LAYERS = 2
+ENC_DROPOUT = 0.5
+DEC_DROPOUT = 0.5
+
+train_iterator, valid_iterator, test_iterator = BucketIterator.splits(
+    (train_data, valid_data, test_data), 
+    batch_size = BATCH_SIZE, 
+    device = device)
+
+enc = Encoder(INPUT_DIM, ENC_EMB_DIM, HID_DIM, N_LAYERS, ENC_DROPOUT)
+dec = Decoder(OUTPUT_DIM, DEC_EMB_DIM, HID_DIM, N_LAYERS, DEC_DROPOUT)
+
+model = Seq2Seq(enc, dec, device).to(device)
+model.apply(init_weights)
+
+
+print(f'The model has {count_parameters(model):,} trainable parameters')
+
+optimizer = optim.Adam(model.parameters())
+TRG_PAD_IDX = TRG.vocab.stoi[TRG.pad_token]
+criterion = nn.CrossEntropyLoss(ignore_index = TRG_PAD_IDX)
+
+
+EPOCH = 10
+CLIP = 1
+best_valid_loss = float('inf')
+for e in tqdm(range(EPOCH)):
+    start_time = time.time()
+    
+    train_loss = train(model, train_iterator, optimizer, criterion, CLIP)
+    valid_loss = evaluate(model, valid_iterator, criterion)
+    
+    end_time = time.time()
+    
+    epoch_mins, epoch_secs = epoch_time(start_time, end_time)
+    
+    if valid_loss < best_valid_loss:
+        best_valid_loss = valid_loss
+        torch.save(model.state_dict(), './weights/{}-model.pth'.format(best_valid_loss))
+    
+    print(f'Epoch: {epoch+1:02} | Time: {epoch_mins}m {epoch_secs}s')
+    print(f'\tTrain Loss: {train_loss:.3f} | Train PPL: {math.exp(train_loss):7.3f}')
+    print(f'\t Val. Loss: {valid_loss:.3f} |  Val. PPL: {math.exp(valid_loss):7.3f}')
+
+model.load_state_dict(torch.load('tut1-model.pt'))
+
+test_loss = evaluate(model, test_iterator, criterion)
+
+print(f'| Test Loss: {test_loss:.3f} | Test PPL: {math.exp(test_loss):7.3f} |')
